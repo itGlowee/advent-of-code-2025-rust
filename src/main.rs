a2d35e9f79c4a96a867df40172d9dcec3dec877d
Glowee <glowee@mailbox.org>
2025-12-21 19:39:04 +0200

    day1 solved

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use core::panic;
+
 
 
 fn main() {
@@ -27,17 +29,47 @@ trait Problem {
 }
 
 
+enum RotDir {
+    Clockwise,
+    Counterclockwise,
+}
+struct Rotation {
+    direction: RotDir,
+    distance: u32,
+}
+
 struct Day01 {
-    input: String,
+    rotations: Vec<Rotation>
 }
 
 impl Problem for Day01 {
     fn day(&self) -> u8 { 1 }
     fn from_input(input: String) -> Self where Self: Sized {
-        Self { input }
+        let mapper = input.lines()
+            .map(|line| Rotation {
+                direction: match line.as_bytes().first() {
+                    Some(b'L') => RotDir::Counterclockwise,
+                    Some(b'R') => RotDir::Clockwise,
+                    Some(_) | None => panic!("Could not get direction from line"),
+                },
+                distance: line[1..].parse().unwrap()
+        });
+        Self {rotations: mapper.collect() }
     }
 
     fn solve(&self) -> Option<String> {
-        Some((&self.input.len()).to_string())
+        let mut current_value: u32 = 50;
+        let mut result: u32 = 0;
+        for rotation in &self.rotations {
+            match rotation.direction {
+                RotDir::Clockwise => current_value = (current_value + (rotation.distance % 100)) % 100,
+                RotDir::Counterclockwise => current_value = (current_value + 100 - (rotation.distance % 100)) % 100,
+            }
+            if current_value == 0 {
+                result += 1;
+            }
+            println!("{}", current_value);
+        }
+        Some(result.to_string())
     }
 }
