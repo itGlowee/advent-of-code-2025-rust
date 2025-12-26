a6a0798cd902d414cfd4348a5f09c3c79ee0fedd
Glowee <glowee@mailbox.org>
2025-12-26 16:16:18 +0200

    day 8 wip

---
+++ src/day8.rs
@@ -0,0 +1,94 @@
+use crate::Problem;
+
+
+const PAIRS: u32 = 10;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Position {
+    x: u32,
+    y: u32,
+    z: u32,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct JunctionBox<'a> {
+    pos: Position,
+    connections: Vec<&'a JunctionBox<'a>>,
+}
+
+impl Position {
+    fn distance(self, rhs: Position) -> f32 {
+        let x = (self.x as f32 - rhs.x as f32).powi(2);
+        let y = (self.y as f32 - rhs.y as f32).powi(2);
+        let z = (self.z as f32 - rhs.z as f32).powi(2);
+        (x + y + z).sqrt()
+    }
+}
+
+
+impl std::ops::Sub<Position> for Position {
+    fn sub(self, rhs: Position) -> Self::Output {
+        Position {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+    type Output = Position;
+}
+
+
+#[derive(Debug, Clone)]
+pub struct Day08 {
+    boxes: Vec<Position>,
+}
+
+impl Problem for Day08 {
+    fn day(&self) -> u8 { 8 }
+    fn from_input(input: String) -> Self where Self: Sized {
+        let mut positions = Vec::new();
+        for line in input.lines() {
+            let num_strings: Vec<u32> = line.split(',').map(|str| str.parse::<u32>().unwrap()).collect();
+            assert!(num_strings.len() == 3);
+            positions.push(Position {
+                x: *num_strings.get(0).unwrap(),
+                y: *num_strings.get(1).unwrap(),
+                z: *num_strings.get(2).unwrap(),
+            });
+        }
+        Day08 { boxes: positions }
+    }
+    fn solve(&self) -> Option<String> {
+        println!("{:?}", self);
+        let mut min_distance = f32::MAX;
+        for _ in 0..PAIRS {
+            for pos1 in &self.boxes {
+                let mut last_pos2: Option<&Position> = None;
+                for pos2 in &self.boxes {
+                    if *pos1 == *pos2 {
+                        continue;
+                    }
+
+                    let d = pos1.distance(*pos2);
+                    if d < min_distance {
+                        last_pos2 = Some(pos2);
+                        min_distance = d;
+                    }
+                }
+
+                // connect
+                match last_pos2 {
+                    Some(last_pos2) => {
+                        println!("Smallest distance found between {:?} and {:?}", pos1, last_pos2);
+                    }
+                    None => (),
+                }
+            }
+            min_distance = f32::MAX;
+        }
+        None
+    }
+    fn solve2(&self) -> Option<String> {
+        None
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod day4;
 mod day5;
 mod day6;
 mod day7;
+mod day8;
 use day1::Day01;
 use day2::Day02;
 use day3::Day03;
@@ -12,6 +13,7 @@ use day4::Day04;
 use day5::Day05;
 use day6::Day06;
 use day7::Day07;
+use day8::Day08;
 
 
 fn main() {
@@ -23,6 +25,7 @@ fn main() {
         Box::new(Day05::new("inputs/day5.txt")),
         Box::new(Day06::new("inputs/day6.txt")),
         Box::new(Day07::new("inputs/day7.txt")),
+        Box::new(Day08::new("inputs/day8example.txt")),
     ];
 
     for p in problems {
